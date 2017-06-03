32a44c259d0e0b16cf9a57309dbc4cd81204a507
Gabrielle C <gabriellelc95@gmail.com>
2017-06-03 10:00:13 -0500

    critical db fix

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
     //database: 'mongodb://localhost:27017/accountability',
-    database: "mongo ds161551.mlab.com:61551/bible-accountability -u 'gabriellelc95' -p 'bibleapp4me'",
+    database: "mongodb://gabriellelc95:bibleapp4me@ds161551.mlab.com:61551/bible-accountability",
     secret: 'mysecret'
 }
